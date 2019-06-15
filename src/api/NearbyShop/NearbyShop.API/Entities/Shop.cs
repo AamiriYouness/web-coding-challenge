de691003907fd1755622adf264799ce679a3c785
Youness Aamiri <158582@supinfo.com>
2019-06-15 18:01:25 +0100

    In this commit i added entities for shops and for users, in my application shops will be retrivied from sql database using an ORM EntityFramework. For users I am using the built-in Idedntity service provied with apsnet core for more secure and managed sessions instead of creating my own bussiness logic.
    Next steps
    
    Now, I need to createa database context for the ORM and seed the
    database with shops.

---
+++ src/api/NearbyShop/NearbyShop.API/Entities/Shop.cs
@@ -0,0 +1,10 @@
+﻿namespace NearbyShop
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public int Distance { get; set; }
+    }
+}
